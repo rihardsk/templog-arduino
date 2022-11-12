1f2f7ae8f6a272fc3bbc651dfbc562f5b17c2d61
Rihards Krišlauks <rihards.krislauks@gmail.com>
2022-11-13 01:09:48 +0200

    Attempt to read the temperature sensor data

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 #![no_std]
 #![no_main]
 
+use dht_sensor::{dht11, DhtReading};
 use panic_halt as _;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+
+    let mut delay = arduino_hal::Delay::new();
+    let mut temp_pin = pins.d2.into_opendrain_high();
+
+    let mut serial = arduino_hal::default_serial!(dp, pins, 115200);
 
     /*
      * For examples (and inspiration), head to
@@ -21,6 +27,20 @@ fn main() -> ! {
     let mut led = pins.d13.into_output();
 
     loop {
+        match dht11::Reading::read(&mut delay, &mut temp_pin) {
+            Ok(dht11::Reading {
+                temperature,
+                relative_humidity,
+            }) => {
+                ufmt::uwriteln!(&mut serial, "{}°, {}% RH", temperature, relative_humidity).unwrap()
+            }
+            Err(e) => match e {
+                dht_sensor::DhtError::PinError(_) => ufmt::uwriteln!(&mut serial, "Error: Pin error").unwrap(),
+                dht_sensor::DhtError::ChecksumMismatch => ufmt::uwriteln!(&mut serial, "Error: Checksum mismatch").unwrap(),
+                dht_sensor::DhtError::Timeout => ufmt::uwriteln!(&mut serial, "Error: Timeout").unwrap(),
+            },
+            // ufmt::uwriteln!(&mut serial, "Error {}", "aa").unwrap(),
+        }
         led.toggle();
         arduino_hal::delay_ms(1000);
     }
