5b30cb0da2951b8b65198dee2aeeec655f5e0224
Rihards Krišlauks <rihards.krislauks@gmail.com>
2023-04-12 22:05:18 +0300

    Testing NaiveDateTime core::fmt::Display implementation

--- device/src/main.rs
+++ device/src/main.rs
@@ -3,6 +3,7 @@
 
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
+use ufmt::{derive::uDebug, uWrite};
 
 use heapless::Vec;
 use panic_halt as _;
@@ -30,7 +31,8 @@ fn main() -> ! {
         ufmt::uwriteln!(&mut serial, "Converting").unwrap();
         arduino_hal::delay_ms(1000);
 
-        // TODO: wtf, for some reason this serialization code works ...
+        // For some reason this serialization code works ...
+        //
         // let message = "hElLo";
         // let bytes = [0x01, 0x10, 0x02, 0x20];
         // let output: postcard::Result<Vec<u8, 11>> = to_vec(&RefStruct {
@@ -38,26 +40,53 @@ fn main() -> ! {
         //     str_s: message,
         // });
 
-        // TODO: ... but if we use this code, then it messes up even other
-        // uwriteln! calls (ok, that was the case previously, now it simply
-        // resets and doesn't get to the next uwriteln)
+        // ... but this doesn't
         //
-        // Not sure how much memory this takes but 64 bytes should be plenty for
-        // serialization, i guess
-        let output: postcard::Result<Vec<u8, 64>> = to_vec(&entry);
+        // NOTE: Not sure how much memory this takes but 64 bytes should be
+        // plenty for serialization, i guess
+        // let output: postcard::Result<Vec<u8, 64>> = to_vec(&entry);
 
-        ufmt::uwriteln!(&mut serial, "Writing").unwrap();
-        arduino_hal::delay_ms(1000);
-        match output {
-            Ok(output) => {
-                for byte in output {
-                    serial.write_byte(byte);
-                }
-                serial.flush();
+        use core::fmt::Write;
+
+        // struct BufWriter {
+        //     buf: [u8; 64],
+        // }
+        // impl Write for BufWriter {
+        //     fn write_str(&mut self, s: &str) -> core::result::Result<(), core::fmt::Error> {
+        //         for (i, b) in s.as_bytes().iter().enumerate() {
+        //             self.buf[i] = *b;
+        //         }
+        //         Ok(())
+        //     }
+        // }
+
+        // let mut buf_writer = BufWriter { buf: [0; 64] };
+
+        struct SerialWriter<'a, T> {
+            serial: &'a mut T,
+        }
+        impl<'a, T> Write for SerialWriter<'a, T> where T: uWrite {
+            fn write_str(&mut self, s: &str) -> core::result::Result<(), core::fmt::Error> {
+                ufmt::uwrite!(&mut self.serial, "{}", s).map_err(|_| core::fmt::Error::default())?;
+                // for (i, b) in s.as_bytes().iter().enumerate() {
+                //     self.buf[i] = *b;
+                // }
+                Ok(())
             }
-            Err(_e) => ufmt::uwriteln!(&mut serial, "Serialization error").unwrap(),
         }
 
+        let mut serial_writer = SerialWriter { serial: &mut serial };
+
+        write!(&mut serial_writer, "{}", entry).unwrap();
+        ufmt::uwriteln!(&mut serial, "").unwrap();
+
+        ufmt::uwriteln!(&mut serial, "Writing").unwrap();
+
+        // arduino_hal::delay_ms(1000);
+        // for byte in buf_writer.buf {
+        //     ufmt::uwriteln!(&mut serial, "{}", byte).unwrap();
+        // }
+        // 062144
         arduino_hal::delay_ms(5000);
     }
 }
